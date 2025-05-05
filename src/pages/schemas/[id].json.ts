8226730f4cfa2fc99190c4ced42dd4187fb78235
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-05-05 15:09:25 +0900

    draft

--- src/pages/schemas/[id].json.ts
+++ src/pages/schemas/[id].json.ts
@@ -1,12 +1,12 @@
 export const prender = false;
 
 import type { APIRoute } from "astro";
-import { getSchemaById } from "./schema-loader";
+import { getSchemaWithId } from "./schema-loader";
 
 // TODO use storage with cloudflare kv
 
-export const GET: APIRoute = ({ params }) => {
-	const schema = getSchemaById(params.id as string);
+export const GET: APIRoute = async ({ params }) => {
+	const schema = await getSchemaWithId(params.id as string);
 	
 	if (!schema) {
 		return new Response(JSON.stringify({ error: "Schema not found" }), {
---
+++ src/pages/schemas/index.ts
@@ -0,0 +1,7 @@
+import type { SchemaId } from './schema-loader';
+
+// Export all schema-related functionality
+export type { 
+  SchemaId
+};
+
