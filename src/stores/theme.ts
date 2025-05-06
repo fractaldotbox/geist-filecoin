2db851ead3c608e894dea6e59d24d74ccbd7c0eb
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-05-06 21:38:51 +0900

    fix: set theme

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -2,18 +2,22 @@ import { atom } from "nanostores";
 
 type Theme = "dark" | "light" | "system";
 
-const storageKey = "vite-ui-theme";
+export const THEME_STORAGE_KEY = "vite-ui-theme";
 
 const initialTheme =
 	typeof window !== "undefined"
-		? (localStorage.getItem(storageKey) as Theme) || "system"
+		? (localStorage.getItem(THEME_STORAGE_KEY) as Theme) || "system"
 		: "system";
 
 export const themeStore = atom<Theme>(initialTheme);
 
 export function setTheme(theme: Theme) {
 	if (typeof window !== "undefined") {
-		localStorage.setItem(storageKey, theme);
+		localStorage.setItem(THEME_STORAGE_KEY, theme);
 		themeStore.set(theme);
+
+		theme === "dark"
+			? document.documentElement.classList.add("dark")
+			: document.documentElement.classList.remove("dark");
 	}
 }
