06e5132b263e7cf27ec4c0dc5e8556d90fba3d9d
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-05-06 21:00:12 +0900

    fix: title

--- src/components/react/ContentHeader.tsx
+++ src/components/react/ContentHeader.tsx
@@ -8,9 +8,10 @@ export function ContentHeader({ title }: ContentHeaderProps) {
 	return (
 		<div className="sticky top-0 z-50 bg-background border-b pb-2 mb-2">
 			<div className="flex justify-between items-center">
-				<h1 className="text-2xl font-bold">
-					<a href="/">Geist</a> | {title}
-				</h1>
+				<div className="text-2xl font-bold">
+					<a href="/">Geist</a>
+				</div>
+				<div className="text-lg font-light">{title}</div>
 				<div className="flex items-center gap-4">
 					<ThemeToggle />
 				</div>
